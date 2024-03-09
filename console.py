43260938de8024be13ec257754ab00ad13021047
vn-vision <vctrngang@gmail.com>
2024-03-09 16:54:58 +0300

    Console now, handle empty line

--- console.py
+++ console.py
@@ -8,12 +8,17 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        ''' command exits the program '''
+        ''' Quit command to exit the program '''
         return True
 
     def do_EOF(self, line):
-        ''' exit the program on  quit signal '''
+        ''' Exit the program on  EOF  signal (Ctrl + D) '''
+        print()
         return True
+
+    def emptyline(self):
+        ''' Do nothing on empty input line '''
+        pass
 
 
 if __name__ == '__main__':
