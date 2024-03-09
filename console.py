ba7367a035fd2ead05d531a83c7505c87006818f
vn-vision <vctrngang@gmail.com>
2024-03-09 16:45:19 +0300

    Console now, pycode

--- console.py
+++ console.py
@@ -2,10 +2,11 @@
 ''' Command line interpreter for the airbnb '''
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     ''' this is the class definition '''
     prompt = '(hbnb) '
-    
+
     def do_quit(self, line):
         ''' command exits the program '''
         return True
@@ -13,6 +14,7 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         ''' exit the program on  quit signal '''
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
